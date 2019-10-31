9a42e075cb6e4c9d3dd7652658b548a2d45853b0
Paul Alexander <myotheremail@oh.rr.com>
2019-10-31 10:59:25 -0400

    Add ability to use backspace to erase digits, Add ability to use result in another operation

--- calculator.js
+++ calculator.js
@@ -1,14 +1,23 @@
 const BUTTONS = document.querySelectorAll('.butts');
 const DISPLAY = document.getElementById('disp');
 let opstring = `0`;
+let interResult;
 let numMode = true;
 let opMode = false;
 let equaledMode = false;
+let backMode = false;
 let opArray = [];
 
 DISPLAY.textContent = opstring;
 
 function formulate(e) {
+    if (this.classList.contains('back') && backMode) {
+        if (/[0-9.]/.test(opstring[opstring.length - 1])) {
+            opstring = opstring.substring(0, opstring.length - 1) || `0`;
+            DISPLAY.textContent = opstring;
+        }
+    }
+
     if (this.classList.contains('nums')) {
         switch (this.id) {
             case '0':
@@ -31,10 +40,11 @@ function formulate(e) {
                     }
                     opstring += `${this.textContent}`;
                     opMode = true;
+                    backMode = true;
                 }
-            }
         }
-        
+    }
+
     if (this.classList.contains('syms')) {
         if (opMode || this.id == 'C') {
             switch (this.id) {
@@ -47,14 +57,30 @@ function formulate(e) {
                 case '*':
                 case '/':
                     opstring += ` ${this.textContent} `;
-                    break;    
+                    break;
                 case 'C':
                     opstring = `0`;
                     opArray = [];
+                    interResult = ``;
                     break;
             }
         }
+
+        if (equaledMode) {
+            switch (this.id) {
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                    opstring = `${interResult} ${this.textContent} `;
+                    equaledMode = false;
+                    break;
+            }
+
+        }
+
         opMode = false;
+        backMode = false;
     }
     DISPLAY.textContent = opstring;
 }
@@ -71,32 +97,33 @@ function operate(arr) {
 
     quotientOrProduct = nArr.findIndex(index => /^[\/*]$/.test(index));
     while (quotientOrProduct != -1) {
-        tempSlice = nArr.slice(quotientOrProduct -1, quotientOrProduct +2);
+        tempSlice = nArr.slice(quotientOrProduct - 1, quotientOrProduct + 2);
         if (tempSlice[1] == '/') {
             tempSlice = DIVIDE(tempSlice);
         }
         else if (tempSlice[1] == '*') {
             tempSlice = MULTIPLY(tempSlice);
         }
-        nArr.splice(quotientOrProduct -1, 3, tempSlice);
+        nArr.splice(quotientOrProduct - 1, 3, tempSlice);
         quotientOrProduct = nArr.findIndex(index => /^[\/*]$/.test(index));
     }
 
     sumOrDifference = nArr.findIndex(index => /^[+-]$/.test(index));
     while (sumOrDifference != -1) {
-        tempSlice = nArr.slice(sumOrDifference -1, sumOrDifference +2);
+        tempSlice = nArr.slice(sumOrDifference - 1, sumOrDifference + 2);
         if (tempSlice[1] == '+') {
             tempSlice = ADD(tempSlice);
         }
         else if (tempSlice[1] == '-') {
             tempSlice = SUBTRACT(tempSlice);
         }
-        nArr.splice(sumOrDifference -1, 3, tempSlice);
+        nArr.splice(sumOrDifference - 1, 3, tempSlice);
         sumOrDifference = nArr.findIndex(index => /^[+-]$/.test(index));
     }
 
-    equaledMode = true;    
-    
+    equaledMode = true;
+    backMode = false;
+    interResult = nArr.join();
     return opstring += ` = ${nArr.join()}`
 }
 
